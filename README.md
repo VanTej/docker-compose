f4e6620a0633c0b3916b67854391509097b2ef83
VanTej <vanessatejedor@gmail.com>
2022-10-20 17:19:16 +0200

    docker-quest3

---
+++ README.md
@@ -0,0 +1,3 @@
+# docker-compose
+
+docker compose -f docker-compose.dev.yml up --build
---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Je suis là, youhouuuuuuuuuuu ! y a qq'un ???? hey ohhhhhhhhhhh....... je me sens seule :(
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
